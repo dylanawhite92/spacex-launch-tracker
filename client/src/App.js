3a0a0cca3b47b08c5a633529709328777b657788
Dylan White <dylanawhite11@gmail.com>
2019-04-10 21:27:35 -0500

    created components folder, added spacex logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Logo from './images/spaceX-Logo.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SpaceX</h1>
+        <img src={Logo} alt="SpaceX" style={{ width : 300, display : 'block', margin : 'auto' }}/>
       </div>
     );
   }
