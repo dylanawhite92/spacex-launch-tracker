71fcb96fcd344391fc27a920cc29fe36a6f82943
Dylan White <dylanawhite11@gmail.com>
2019-04-12 13:40:14 -0500

    created Launches component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
-import Logo from './images/spaceX-Logo.png'
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import "./App.css";
+import Logo from "./images/spaceX-Logo.png";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img src={Logo} alt="SpaceX" style={{ width : 300, display : 'block', margin : 'auto' }}/>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img
+            src={Logo}
+            alt="SpaceX"
+            style={{ width: 300, display: "block", margin: "auto" }}
+          />
+        </div>
+      </ApolloProvider>
     );
   }
 }
---
+++ client/src/components/Launches/Launches.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+export class Launches extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Launches
