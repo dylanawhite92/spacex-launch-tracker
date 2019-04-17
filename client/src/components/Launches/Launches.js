82f66514e58678fe3ba455196d3417c8445a8984
Dylan White <dylanawhite11@gmail.com>
2019-04-17 15:43:14 -0500

    able to map over data in LaunchItem.js

--- client/src/components/Launches/Launches.js
+++ client/src/components/Launches/Launches.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
+import LaunchItem from "../LaunchItem/LaunchItem";
 
 // Store GraphQL query in variable
 // the gql tag makes the actual queries
@@ -18,21 +19,26 @@ const LAUNCHES_QUERY = gql`
 export class Launches extends Component {
   render() {
     return (
-      <div>
+      <>
         <h1 className="display-4 my-3">Launches</h1>
         {/* Set query prop to LAUNCHES_QUERY */}
         <Query query={LAUNCHES_QUERY}>
           {({ loading, error, data }) => {
-            // Placeholder for loading spinner, error handling, and return data
+            // Placeholder for loading spinner, error handling, and map through data
             if (loading) return <h4>Loading...</h4>;
             if (error) console.log(error);
 
-            console.log(data);
-
-            return <h1>Test</h1>;
+            return <>
+              {
+                // For each launch, render LaunchItem component
+                data.launches.map(launch => (
+                  <LaunchItem key={launch.flight_number} launch={launch}/>
+                ))
+              }
+            </>;
           }}
         </Query>
-      </div>
+      </>
     );
   }
 }
