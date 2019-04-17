35303996fa5a457f7ca89d0c8206c2fe4887217d
Dylan White <dylanawhite11@gmail.com>
2019-04-17 15:49:45 -0500

    LaunchItem.js component rendering correctly

--- client/src/components/LaunchItem/LaunchItem.js
+++ client/src/components/LaunchItem/LaunchItem.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
-export default function LaunchItem(props) {
+// Pass in props of mapped data
+export default function LaunchItem({ launch: {flight_number, mission_name, launch_date_local, launch_success} }) {
 
-
-    console.log(props.launch);
     return (
-    <div>
-      test
+    <div className="card card-body mb-3">
+      <div className="row">
+        <div className="col-md-9">
+            <h4>Mission: { mission_name }</h4>
+            {/* Moment.js will be brought in to format date */}
+            <p>Date: { launch_date_local }</p>
+        </div>
+        <div className="col-md-3">
+            {/* Placeholder until react-router is in place */}
+            <button className="btn btn-secondary">Launch Details</button>
+        </div>
+      </div>
     </div>
   );
 };
