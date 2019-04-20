be9d135456406ce1e553b4cd5fc3a3ec8a51270e
Dylan White <dylanawhite11@gmail.com>
2019-04-20 15:12:03 -0500

    modified Launch.js

--- client/src/components/Launch/Launch.js
+++ client/src/components/Launch/Launch.js
@@ -1,4 +1,25 @@
 import React, { Component } from 'react'
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+
+const LAUNCH_QUERY = gql`
+  query LaunchQuery($flight_number: Int!) {
+    launch(flight_number: $flight_number) {
+      flight_number,
+      mission_name,
+      launch_year,
+      launch_success,
+      launch_date_local,
+      rocket {
+        rocket_id,
+        rocket_name,
+        rocket_type
+      }
+    }
+  }
+`;
 
 export class Launch extends Component {
   render() {
