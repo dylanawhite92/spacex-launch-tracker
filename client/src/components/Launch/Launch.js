95f50fb7c72d80cb532f958cd2de9370415b5dc4
Dylan White <dylanawhite11@gmail.com>
2019-04-20 15:33:00 -0500

    modifying Launch.js, keying in launch data

--- client/src/components/Launch/Launch.js
+++ client/src/components/Launch/Launch.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
-import { Link } from 'react-router-dom';
-import classNames from 'classnames';
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
 
 const LAUNCH_QUERY = gql`
   query LaunchQuery($flight_number: Int!) {
     launch(flight_number: $flight_number) {
-      flight_number,
-      mission_name,
-      launch_year,
-      launch_success,
-      launch_date_local,
+      flight_number
+      mission_name
+      launch_year
+      launch_success
+      launch_date_local
       rocket {
-        rocket_id,
-        rocket_name,
+        rocket_id
+        rocket_name
         rocket_type
       }
     }
@@ -23,12 +23,46 @@ const LAUNCH_QUERY = gql`
 
 export class Launch extends Component {
   render() {
+    // Use destructuring to pull params out of props
+    let { flight_number } = this.props.match.params;
+    // Convert to int, will return error if string
+    flight_number = parseInt(flight_number);
     return (
-      <div>
-        <h1>Launch</h1>
-      </div>
-    )
+      <>
+        <Query query={LAUNCH_QUERY} variables={{ flight_number }}>
+          {({ loading, error, data }) => {
+            // Placeholder for loading spinner, error handling, and return launch detail data
+            if (loading) return <h4>Loading...</h4>;
+            if (error) console.log(error);
+
+            // Destructuring launch details for cleaner code
+            const {
+              mission_name,
+              flight_number,
+              launch_year,
+              launch_success,
+              rocket: { rocket_id, rocket_name, rocket_type }
+            } = data.launch;
+
+            return (
+              <div>
+                <h1 className="display-4 my-3">
+                  <span className="text-dark">Mission:</span>{" "}
+                  {mission_name}
+                </h1>
+                <h4 className="mb-3">Launch Details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">
+                    Flight Number : {flight_number}
+                  </li>
+                </ul>
+              </div>
+            );
+          }}
+        </Query>
+      </>
+    );
   }
 }
 
-export default Launch
+export default Launch;
