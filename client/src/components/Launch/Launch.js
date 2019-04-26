c0164c53458a2cacd19d120ddcf006a1fb4e5b5f
Dylan White <dylanawhite11@gmail.com>
2019-04-26 15:31:56 -0500

    modified Launch.js

--- client/src/components/Launch/Launch.js
+++ client/src/components/Launch/Launch.js
@@ -70,6 +70,13 @@ export class Launch extends Component {
                     </span>
                   </li>
                 </ul>
+
+                <h4 className="my-3">Rocket Details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">Rocket ID: {rocket_id}</li>
+                  <li className="list-group-item">Rocket Name: {rocket_name}</li>
+                  <li className="list-group-item">Rocket Type: {rocket_type}</li>
+                </ul>
               </div>
             );
           }}
